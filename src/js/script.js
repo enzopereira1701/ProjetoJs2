303e62dc948e48b4264fa75cb44a3e696ed5fd02
enzo <enzopereira1701@gmail.com>
2026-04-28 11:20:05 -0300

    FEAT: Métodos trim, split, includes e join

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+
+
+// LIMPEZA 
+// trim = um método que esta ligado ao ao objeto, ele remove espaços colocados 
+// pelo usuário antes e depois da palavra 
+
+const email ="   email@email.com";
+const email_limpo = email.trim();
+console.log(email_limpo)
+
+// VALIDAÇÂO 
+// includes = o que você colocar dentro, ve se tem ou não, se tiver é 
+// válido, se não, é inválido
+if(email_limpo.includes("@" && ".com")){
+    console.log("E-mail válido")
+}else{
+    console.log("E-mail inválido")
+}
+
+// TRANSFORMAÇÃO DE TEXTO 
+
+// LowerCase = transforma o teto para ,omisculo 
+const titulo_artigo="Como aprender JAVA RAIZ"
+const texto1 = titulo_artigo.toLowerCase()
+console.log(texto1)
+
+// split = transforma o etxto em arrays 
+const texto2 = titulo_artigo.split(" ");
+console.log(texto2)
+
+// join = junto tudo em uma string (unico texto)
+const texto3 = texto2.join(" ");
+console.log(texto3)
